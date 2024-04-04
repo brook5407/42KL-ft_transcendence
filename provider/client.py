bbec90047b0a78ae7fa3a82c64622412a066a94e
brookchin <chunyong96@gmail.com>
2024-04-05 00:18:06 +0800

    move API ID and Secret to core settings.py

--- provider/client.py
+++ provider/client.py
@@ -14,15 +14,13 @@ class FortyTwoOAuth2Client(OAuth2Client):
 
     base_url = f'{settings.OAUTH_SERVER_BASEURL}/oauth/authorize'
     token_url = f'{settings.OAUTH_SERVER_BASEURL}/oauth/token'
-    client_id = 'u-s4t2ud-c54860141cd7e4afbb2b7f4f6fdaed8483360fba16e5acf438a021964386f8e6'
-    client_secret = "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"
     redirect_uri = 'http://127.0.0.1:8000/42/login/callback/'
     response_type = 'code'
     
     def get_redirect_url(self, authorization_url, extra_params):
         
         params = {
-            'client_id': self.client_id,
+            'client_id': self.consumer_key,
             'redirect_uri': self.callback_url,
             'response_type': self.response_type,
         }
@@ -47,8 +45,8 @@ class FortyTwoOAuth2Client(OAuth2Client):
 
         data = {
             "grant_type": "authorization_code",
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
+            "client_id": self.consumer_key,
+            "client_secret": self.consumer_secret,
             "code": code,
             "redirect_uri": self.callback_url
         }
