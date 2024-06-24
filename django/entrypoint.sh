bd4b99bce7261fe0f24df3ae0491cb0cef136e97
brookchin <chunyong96@gmail.com>
2024-06-24 20:36:42 +0800

    configure railway settings

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-``
+
+RUN_PORT="${PORT:-8000}"
+
 # Collect static files
 python manage.py collectstatic --noinput
 
 # Apply database migrations
 python manage.py migrate
 
-
 # Start the server
-exec "$@"
+daphne -b 0.0.0.0 -p $RUN_PORT core.asgi:application
--- django/src/core/settings.py
+++ django/src/core/settings.py
@@ -94,7 +94,7 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 
 if APP_ENV == 'dev' or DB_ENGINE is None:
-	DATABASES = {
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
@@ -102,18 +102,18 @@ if APP_ENV == 'dev' or DB_ENGINE is None:
     }
 else:
     DATABASES = {
-    "default": {
-        "ENGINE": DB_ENGINE,
-        "NAME": DB_NAME,
-        "USER": DB_USER,
-        "PASSWORD": DB_PASSWORD,
-        "HOST": DB_HOST,
-        "PORT": DB_PORT,
-    },
-}
+        "default": {
+            "ENGINE": DB_ENGINE,
+            "NAME": DB_NAME,
+            "USER": DB_USER,
+            "PASSWORD": DB_PASSWORD,
+            "HOST": DB_HOST,
+            "PORT": DB_PORT,
+        },
+    }
 
 if APP_ENV == 'dev' or DB_ENGINE is None:
-	DATABASES['default'] = {
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
