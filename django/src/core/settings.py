32afd04280cc49cbf90552c58c3c7bdb722e0994
@Brook <chunyong96@gmail.com>
2024-06-26 01:12:38 +0000

    remove duplicate database settings

--- django/src/core/settings.py
+++ django/src/core/settings.py
@@ -113,12 +113,6 @@ else:
         },
     }
 
-if APP_ENV == 'dev' or DB_ENGINE is None:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
