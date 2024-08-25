2f02cc6b15967b9489704fc5fe98c6798f01e6c5
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-08-25 14:10:32 +0800

    feat: drawer back button

--- django/src/static/js/spa/drawer.js
+++ django/src/static/js/spa/drawer.js
@@ -7,7 +7,54 @@ import { FriendList } from './components/drawer/friend-list.js';
 import { FriendRequests } from './components/drawer/friend-requests.js';
 import { SearchFriend } from './components/drawer/search-friend.js';
 
+class DrawerStack {
+	constructor() {
+		/**
+		 * The stack of drawers.
+		 * every element in the stack stores the drawer name and drawer url
+		 * @type {Array<{drawerName: string, drawerUrl: string}>}
+		 */
+		this.stack = [];
+	}
+
+	/**
+	 * Push a drawer onto the stack.
+	 * @param {string} drawerName - The name of the drawer.
+	 * @param {string} drawerUrl - The URL of the drawer.
+	 */
+	push(drawerName, drawerUrl) {
+		this.stack.push({
+			drawerName,
+			drawerUrl,
+		});
+	}
+
+	/**
+	 * Pop a drawer from the stack.
+	 * @returns {void}
+	 */
+	pop() {
+		this.stack.pop();
+	}
+
+	empty() {
+		this.stack = [];
+	}
+
+	/**
+	 * Get the current drawer from the stack.
+	 * @returns {{drawerName: string, drawerUrl: string}|undefined} The current drawer or undefined if the stack is empty.
+	 */
+	getCurrentDrawer() {
+		if (this.stack.length === 0) {
+			return undefined;
+		}
+		return this.stack[this.stack.length - 1];
+	}
+}
+
 let currentDrawer = null;
+const drawerStack = new DrawerStack();
 
 export const DRAWERS = {
 	profile: Profile,
@@ -26,20 +73,27 @@ document.body.addEventListener('click', (e) => {
 		const drawerName = e.target.getAttribute('data-drawer');
 		const drawerUrl = e.target.getAttribute('data-drawer-url') || '';
 		openDrawer(drawerName, { url: drawerUrl });
+	} else if (e.target.matches('.drawer-back-btn')) {
+		// open the previous drawer
+		e.preventDefault();
+		currentDrawer?.destroy();
+		drawerStack.pop();
+		const drawerToOpen = drawerStack.getCurrentDrawer();
+		if (drawerToOpen) {
+			openDrawer(
+				drawerToOpen.drawerName,
+				{ url: drawerToOpen.drawerUrl },
+				false
+			);
+		}
 	}
-});
-
-// custom event to open drawer
-document.addEventListener('open-drawer', (e) => {
-	const drawerName = e.detail.drawerName;
-	openDrawer(drawerName, e.detail.data);
 });
 
 function dispatchDrawerOpenedEvent(e = null) {
 	document.dispatchEvent(new CustomEvent('drawer-opened', e));
 }
 
-export async function openDrawer(drawerName, data = {}) {
+export async function openDrawer(drawerName, data = {}, pushStack = true) {
 	const drawerClass = DRAWERS[drawerName];
 	const drawer = new drawerClass({
 		url: data.url,
@@ -48,32 +102,38 @@ export async function openDrawer(drawerName, data = {}) {
 
 	console.log('drawerName:', drawerName);
 
-	if (drawer) {
-		currentDrawer = drawer;
-		const element = await drawer.render();
-		DRAWER_CONTAINER.innerHTML = '';
-		DRAWER_CONTAINER.appendChild(element);
-		setTimeout(() => {
-			activateDrawer();
-			dispatchDrawerOpenedEvent({
-				detail: { drawerName },
-			});
-		}, 100);
-		return element;
-	} else {
+	if (!drawer) {
 		console.error('Drawer not found:', drawerName);
+		return;
 	}
+
+	currentDrawer = drawer;
+	if (pushStack) {
+		drawerStack.push(drawerName, data.url);
+		console.log('drawerStack new length:', drawerStack.stack.length);
+	}
+	const element = await drawer.render();
+	DRAWER_CONTAINER.innerHTML = '';
+	DRAWER_CONTAINER.appendChild(element);
+	setTimeout(() => {
+		activateDrawer();
+		dispatchDrawerOpenedEvent({
+			detail: { drawerName },
+		});
+	}, 100);
+	return element;
 }
 
 export function closeDrawer() {
 	const drawerOverlay = document.getElementById('drawerOverlay');
 	const drawer = document.getElementById('drawer');
-	drawerOverlay.classList.remove('drawer-active');
-	drawer.classList.remove('drawer-active');
+	drawerOverlay?.classList.remove('drawer-active');
+	drawer?.classList.remove('drawer-active');
 	setTimeout(() => {
 		DRAWER_CONTAINER.innerHTML = '';
 		currentDrawer?.destroy();
 	}, 500);
+	drawerStack.empty();
 }
 
 function activateDrawer() {
@@ -82,13 +142,10 @@ function activateDrawer() {
 	drawerOverlay.classList.add('drawer-active');
 	drawer.classList.add('drawer-active');
 
-	// document
-	// 	.getElementById('closeDrawerBtn')
-	// 	.addEventListener('click', closeDrawer);
-
 	document
 		.getElementById('drawerOverlay')
 		.addEventListener('click', closeDrawer);
 }
 
+window.openDrawer = openDrawer;
 window.closeDrawer = closeDrawer;
