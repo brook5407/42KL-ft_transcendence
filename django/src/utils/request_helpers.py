a2035735326e0e2993466438c86514c24c24a14f
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-09-19 03:20:59 +0800

    fix: jwt cookie bugs

--- django/src/utils/request_helpers.py
+++ django/src/utils/request_helpers.py
@@ -1,5 +1,20 @@
 from django.http import HttpRequest
+from functools import wraps
+from django.shortcuts import render, redirect
+import requests
+from django.conf import settings
 
 
 def is_ajax_request(request: HttpRequest):
 	return request.headers.get('x-requested-with') == 'XMLHttpRequest'
+
+
+def authenticated_view(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('index')
+        
+        # User is authenticated, proceed with the view
+        return view_func(request, *args, **kwargs)
+    return wrapper
