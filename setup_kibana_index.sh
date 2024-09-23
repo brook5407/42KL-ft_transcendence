27af42ef4dec2d439cb2afda71c6e2875f009875
brook5407 <chunyong96@gmail.com>
2024-09-23 09:21:18 +0000

    chore: move the command to script

--- setup_kibana_index.sh
+++ setup_kibana_index.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# Wait until Kibana is healthy
 until $(curl --output /dev/null --silent --head --fail http://kibana:5601/api/status); do
     printf '.'
     sleep 5
 done
 
-# Run the curl command to create the index pattern
 curl -XPOST -D- "http://kibana:5601/api/saved_objects/index-pattern" \
     -H "Content-Type: application/json" \
     -H "kbn-version: 6.1.0" \
-    -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
-
+    -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}' && \
 echo "Index pattern created."
