b90c4bc4bfd530e52878d4c5b2c0b48c6e4450f9
Brook <chuntong96@gmail.com>
2024-09-23 11:31:24 +0800

    chore: update url

--- openssl-cert/tools/script.sh
+++ openssl-cert/tools/script.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/cert/cert.pem" ]; then
 	echo "Generating certificate"
-    openssl req -x509 -newkey rsa:4096 -keyout /cert/key.pem -out /cert/cert.pem -days 365 -nodes -subj "/C=Mr/L=KL/O=42kl/OU=student/CN=ft-transcendence.42.fr"
+    openssl req -x509 -newkey rsa:4096 -keyout /cert/key.pem -out /cert/cert.pem -days 365 -nodes -subj "/C=Mr/L=KL/O=42kl/OU=student/CN=aispong.brookchin.tech"
 fi
 
 if openssl x509 -checkend 86400 -noout -in /cert/cert.pem
@@ -10,5 +10,5 @@ then
 	echo "Valid certificate is exist"
 else
 	echo "Re-generating certificate"
-	openssl req -x509 -newkey rsa:4096 -keyout /cert/key.pem -out /cert/cert.pem -days 365 -nodes -subj "/C=Mr/L=KL/O=42kl/OU=student/CN=ft-transcendence.42.fr"
+	openssl req -x509 -newkey rsa:4096 -keyout /cert/key.pem -out /cert/cert.pem -days 365 -nodes -subj "/C=Mr/L=KL/O=42kl/OU=student/CN=aispong.brookchin.tech"
 fi
