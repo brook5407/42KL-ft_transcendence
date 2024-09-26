6cc6bb54cac4185e0493a8cc558d2078696117f4
brook5407 <chunyong96@gmail.com>
2024-09-26 01:19:30 +0000

    refactore: update docker compose

--- kibana/setup_kibana_index.sh
+++ kibana/setup_kibana_index.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-until $(curl --output /dev/null --silent --head --fail http://kibana:5601/api/status); do
-    printf '.'
-    sleep 5
-done
+sleep 100
 
 curl -XPOST -D- "http://kibana:5601/api/saved_objects/index-pattern" \
     -H "Content-Type: application/json" \
     -H "kbn-version: 6.1.0" \
     -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}' && \
-echo "Index pattern created."
+    
+echo "\r\nIndex pattern created."
